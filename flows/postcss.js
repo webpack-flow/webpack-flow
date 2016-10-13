d0c25fc005214f9c1b9aeb06e525c4abe0975758
EGOIST <0x142857@gmail.com>
2016-10-13 12:07:30 +0800

    add postcss flow

---
+++ flows/postcss.js
@@ -0,0 +1,19 @@
+module.exports = options => {
+  const test = options.test || /\.css$/
+  const loader = options.loader || 'style!css!postcss'
+
+  delete options.test
+  delete options.loader
+
+  return {
+    module: {
+      loaders: [
+        {
+          test,
+          loader
+        }
+      ]
+    },
+    postcss: options
+  }
+}
