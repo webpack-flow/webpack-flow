8c69cf7a1a8aa58b3706f770f28d5c32d9d7c8d8
EGOIST <0x142857@gmail.com>
2016-10-25 02:00:07 +0800

    remove meow

--- flows/command.js
+++ flows/command.js
@@ -1,7 +1,5 @@
 const flow = require('../flow')
 
 module.exports = (...flows) => {
-  const meow = require('meow')
-  const cli = meow()
-  return command => cli.input[0] === command ? flow(flows) : null
+  return command => process.argv[2] === command ? flow(flows) : null
 }
